8aef3ce7cf83d92a71b5cb3972924247d50bb7c5
vbrunoro <vfmbrunoro@gmail.com>
2024-09-11 18:39:05 -0300

    feat: Add jwt

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,8 +1,13 @@
 import { FastifyInstance } from 'fastify'
 import { register } from './controllers/users/register'
 import { authenticate } from './controllers/users/authenticate'
+import { profile } from './controllers/users/profile'
+import { verifyJwt } from './middlewares/verify-jwt'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
+
+  /** Authenticated */
+  app.get('/me', { onRequest: [verifyJwt] }, profile)
 }
